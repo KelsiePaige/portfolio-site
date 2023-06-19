3280cf48a0769338b65b8a583841400cfdb1960a
kelsie_b <kelsie.p.brown@gmail.com>
2023-06-18 20:33:58 -0400

    Updates to styles and files

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// const toggleBtn = document.querySelector('.toggle_btn');
+// const toggleBtnImage = document.querySelector('.toggle_btn img');
+// const dropDownMenu = document.querySelector('.dropdown_menu');
+
+// toggleBtn.onclick = function () {
+//     dropDownMenu.classList.toggle('open');
+
+// }
